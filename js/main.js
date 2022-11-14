8dd9a2bd4aa450289bb2c9178cc292c97bf17134
Israel CHIZUNGU <chizunguisrael@gmail.com>
2022-11-15 00:30:27 +0200

    Fix js and html code

--- js/main.js
+++ js/main.js
@@ -4,19 +4,23 @@ document.getElementById("hamburger").addEventListener('click', (e)=>{
   e.preventDefault();
 })
 
+// click on Close hamburger
 document.getElementById("mobile-drawer-close-icon").addEventListener('click', (e)=>{
   document.querySelector('.mobile-menu-drawer').setAttribute("style", "visibility: hidden");
   e.preventDefault();
 })
 
+// click on portfolio
 document.getElementById("drawer-nav-url").addEventListener('click', (e)=>{
   document.querySelector('.mobile-menu-drawer').setAttribute("style", "visibility:hidden");
 })
 
+// click on about section menu
 document.getElementById("drawer-nav-url2").addEventListener('click', (e)=>{
   document.querySelector('.mobile-menu-drawer').setAttribute("style", "visibility:hidden");
 })
 
+// click on contact
 document.getElementById("drawer-nav-url3").addEventListener('click', (e)=>{
   document.querySelector('.mobile-menu-drawer').setAttribute("style", "visibility:hidden");
 })
