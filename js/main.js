662aa087cc8d6009dca0323439353481e1c7e859
Israel CHIZUNGU <chizunguisrael@gmail.com>
2022-11-15 00:54:00 +0200

    Clear js code

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
 document.getElementById('hamburger').addEventListener('click', (e) => {
-    // alert('Click on hamburger')
+  // alert('Click on hamburger')
   document.querySelector('.mobile-menu-drawer').setAttribute('style', 'visibility:visible');
   e.preventDefault();
-  });
+});
 
-// click on Close hamburger
+  // click on Close hamburger
 document.getElementById('mobile-drawer-close-icon').addEventListener('click', (e) => {
   document.querySelector('.mobile-menu-drawer').setAttribute('style', 'visibility: hidden');
   e.preventDefault();
-  });
+});
 
-// click on portfolio
+  // click on portfolio
 document.getElementById('drawer-nav-url').addEventListener('click', (e) => {
   document.querySelector('.mobile-menu-drawer').setAttribute('style', 'visibility:hidden');
   e.preventDefault();
-  });
+});
 
-// click on about section menu
+  // click on about section menu
 document.getElementById('drawer-nav-url2').addEventListener('click', (e) => {
   document.querySelector('.mobile-menu-drawer').setAttribute('style', 'visibility:hidden');
   e.preventDefault();
-  });
+});
 
-// click on contact
+  // click on contact
 document.getElementById('drawer-nav-url3').addEventListener('click', (e) => {
   document.querySelector('.mobile-menu-drawer').setAttribute('style', 'visibility:hidden');
   e.preventDefault();
-  });
+});
