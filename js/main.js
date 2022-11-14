a694199697f0698bc4de18aceac8b35808549b23
Israel CHIZUNGU <chizunguisrael@gmail.com>
2022-11-15 00:41:50 +0200

    Fix js and css linters errors

--- js/main.js
+++ js/main.js
@@ -1,26 +1,34 @@
-document.getElementById("hamburger").addEventListener('click', (e)=>{
-  // alert('Click on hamburger')
-  document.querySelector('.mobile-menu-drawer').setAttribute("style", "visibility:visible");
-  e.preventDefault();
-})
+document.getElementById('hamburger').addEventListener('click',
+  (e) => {
+    // alert('Click on hamburger')
+    document.querySelector('.mobile-menu-drawer').setAttribute('style', 'visibility:visible');
+    e.preventDefault();
+  })
 
 // click on Close hamburger
-document.getElementById("mobile-drawer-close-icon").addEventListener('click', (e)=>{
-  document.querySelector('.mobile-menu-drawer').setAttribute("style", "visibility: hidden");
-  e.preventDefault();
-})
+document.getElementById('mobile-drawer-close-icon').addEventListener('click',
+  (e) => {
+    document.querySelector('.mobile-menu-drawer').setAttribute('style', 'visibility: hidden');
+    e.preventDefault();
+  })
 
 // click on portfolio
-document.getElementById("drawer-nav-url").addEventListener('click', (e)=>{
-  document.querySelector('.mobile-menu-drawer').setAttribute("style", "visibility:hidden");
-})
+document.getElementById('drawer-nav-url').addEventListener('click',
+  (e) => {
+    document.querySelector('.mobile-menu-drawer').setAttribute('style', 'visibility:hidden');
+    e.preventDefault();
+  })
 
 // click on about section menu
-document.getElementById("drawer-nav-url2").addEventListener('click', (e)=>{
-  document.querySelector('.mobile-menu-drawer').setAttribute("style", "visibility:hidden");
-})
+document.getElementById('drawer-nav-url2').addEventListener('click',
+  (e) => {
+    document.querySelector('.mobile-menu-drawer').setAttribute('style', 'visibility:hidden');
+    e.preventDefault();
+  })
 
 // click on contact
-document.getElementById("drawer-nav-url3").addEventListener('click', (e)=>{
-  document.querySelector('.mobile-menu-drawer').setAttribute("style", "visibility:hidden");
-})
+document.getElementById('drawer-nav-url3').addEventListener('click',
+  (e) => {
+    document.querySelector('.mobile-menu-drawer').setAttribute('style', 'visibility:hidden');
+    e.preventDefault();
+  })
