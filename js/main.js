56715ed87173411b47a885b627c0cf52b541e4c5
Israel CHIZUNGU <chizunguisrael@gmail.com>
2022-11-15 01:43:52 +0200

    Fix js issue

--- js/main.js
+++ js/main.js
@@ -7,23 +7,19 @@ document.getElementById('hamburger').addEventListener('click', (e) => {
 // click on Close hamburger
 document.getElementById('mobile-drawer-close-icon').addEventListener('click', (e) => {
   document.querySelector('.mobile-menu-drawer').setAttribute('style', 'visibility: hidden');
-  e.preventDefault();
 });
 
 // click on portfolio
 document.getElementById('drawer-nav-url').addEventListener('click', (e) => {
   document.querySelector('.mobile-menu-drawer').setAttribute('style', 'visibility:hidden');
-  e.preventDefault();
 });
 
 // click on about section menu
 document.getElementById('drawer-nav-url2').addEventListener('click', (e) => {
   document.querySelector('.mobile-menu-drawer').setAttribute('style', 'visibility:hidden');
-  e.preventDefault();
 });
 
 // click on contact
 document.getElementById('drawer-nav-url3').addEventListener('click', (e) => {
   document.querySelector('.mobile-menu-drawer').setAttribute('style', 'visibility:hidden');
-  e.preventDefault();
 });
