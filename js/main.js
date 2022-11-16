666baa84da39f0a9f6fa7dc7eddf22f2abb6698e
Israel CHIZUNGU <chizunguisrael@gmail.com>
2022-11-17 00:02:08 +0200

    Fix modal issues

--- js/main.js
+++ js/main.js
@@ -23,3 +23,27 @@ document.getElementById('drawer-nav-url2').addEventListener('click', () => {
 document.getElementById('drawer-nav-url3').addEventListener('click', () => {
   document.querySelector('.mobile-menu-drawer').setAttribute('style', 'visibility:hidden');
 });
+
+var modal = document.getElementById("modal");
+var btn = document.getElementById("showModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// openning the modal
+btn.addEventListener('click', (e)=>{
+  modal.style.display = "block";
+  // e.preventDefault();
+})
+
+// closing the modal if the user clicks anywhere outside the modal
+window.onclick = (event)=>{
+  if(event.target == modal){
+    modal.style.display = "none";
+  }
+}
