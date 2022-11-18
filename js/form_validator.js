680f408c59c44086cb4b75e5a50c7baadc1f2b09
Israel CHIZUNGU <chizunguisrael@gmail.com>
2022-11-18 18:55:14 +0200

    Clear js code

--- js/form_validator.js
+++ js/form_validator.js
@@ -29,6 +29,7 @@ function validateEmail(input, requiredMsg, invalidMsg) {
     return false;
   }
   // validate email format
+  //eslint-disable-next-line
   const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const email = input.value.trim();
   if (!emailRegex.test(email)) {
@@ -37,14 +38,14 @@ function validateEmail(input, requiredMsg, invalidMsg) {
   } if (email.toLowerCase() === email) {
     return true;
   }
-   return showError(input, invalidMsg);
-  }
+  return showError(input, invalidMsg);
+}
 
 const form = document.querySelector('#contact-form');
 const EMAIL_REQUIRED = 'Please enter your email';
 const EMAIL_INVALID = 'Please enter a correct email address format !';
 
-form.addEventListener('submit',  (event) =>{
+form.addEventListener('submit',(event) => {
   // validate the form
   const emailValid = validateEmail(form.elements['.email'], EMAIL_REQUIRED, EMAIL_INVALID);
   if (!emailValid) {
