d9fb13db81ad0c2a0759c601ce7d49537e8f8e2e
Israel CHIZUNGU <chizunguisrael@gmail.com>
2022-12-16 19:15:06 +0200

    Add new projects to the portfolio

---
+++ js/mobile-menu.js
@@ -0,0 +1,28 @@
+let hamburgerBtn = document.getElementById("hamburger");
+let closeDrawerBtn =document.getElementById("exit-button");
+
+hamburgerBtn.addEventListener("click", (e)=>{
+  document.querySelector('.mobile-menu').setAttribute('style', 'visibility:visible');
+  e.preventDefault();
+})
+
+closeDrawerBtn.addEventListener("click", (e)=>{
+  document.querySelector('.mobile-menu').setAttribute('style', 'visibility:hidden');
+  e.preventDefault();
+})
+
+
+// click on any menu
+document.getElementById('menu-link-portfolio').addEventListener('click', () => {
+  document.querySelector('.mobile-menu').setAttribute('style', 'visibility:hidden');
+});
+
+// click on any menu
+document.getElementById('menu-link-about').addEventListener('click', () => {
+  document.querySelector('.mobile-menu').setAttribute('style', 'visibility:hidden');
+});
+
+// click on any menu
+document.getElementById('menu-link-contact').addEventListener('click', () => {
+  document.querySelector('.mobile-menu').setAttribute('style', 'visibility:hidden');
+});
